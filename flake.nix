f4e97998ca1ecb11cc1a721ab61593e7a185c05f
Gareth Eddies <ghedwards@gmail.com>
2025-02-12 20:39:16 +1000

    Add flake.nix / lock

---
+++ flake.nix
@@ -0,0 +1,86 @@
+{
+  description = "A tree-sitter grammar for Cfml";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      version = "0.2.0";
+
+      # Matches pkgs.tree-sitter
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      pkgsFor = nixpkgs.lib.genAttrs supportedSystems (system: import nixpkgs {
+        inherit system;
+      });
+
+      forAllSystems = fn: nixpkgs.lib.genAttrs supportedSystems (system: fn rec {
+        inherit system;
+        pkgs = pkgsFor.${system};
+        inherit (pkgs) lib;
+      });
+    in
+    {
+      overlays = {
+        tree-sitter-grammars = final: prev: {
+          tree-sitter-grammars = prev.tree-sitter-grammars // {
+            tree-sitter-cfml = self.packages.${prev.system}.tree-sitter-cfml;
+          };
+        };
+      };
+
+      packages = forAllSystems ({ system, pkgs, ... }: {
+        tree-sitter-cfml = pkgs.callPackage (nixpkgs + "/pkgs/development/tools/parsing/tree-sitter/grammar.nix") { }
+          {
+            language = "cfml";
+            src = ./.;
+            inherit version;
+          };
+
+        default = self.packages.${system}.tree-sitter-cfml;
+      });
+
+      devShells = forAllSystems ({ system, pkgs, ... }: {
+        default = pkgs.mkShell {
+          inputsFrom = builtins.concatMap builtins.attrValues [
+            self.checks.${system}
+            self.packages.${system}
+          ];
+          env = {
+            npm_config_build_from_source = true;
+          };
+        };
+      });
+
+      checks = forAllSystems ({ pkgs, ... }: {
+        default = pkgs.stdenv.mkDerivation {
+          pname = "tree-sitter-cfml-test";
+          inherit version;
+
+          src = self;
+
+          nativeBuildInputs = [
+            pkgs.nodejs_latest
+            pkgs.tree-sitter
+            pkgs.clang
+          ];
+
+          buildPhase = ''
+            echo 'Running corpus tests'
+            HOME=$(mktemp -d) tree-sitter test
+          '';
+
+          # Write nothing to the store
+          installPhase = "touch $out";
+        };
+      });
+    };
+}
