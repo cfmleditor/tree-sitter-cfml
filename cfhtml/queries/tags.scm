b04bb8fb637dd06ffd0e429f7f25c767a5cfd702
Gareth Edwards <ghedwards@gmail.com>
2026-03-15 21:25:08 +1000

    Update tags

--- cfhtml/queries/tags.scm
+++ cfhtml/queries/tags.scm
@@ -1,2 +1,28 @@
+; Function definitions
+(function_declaration
+  name: (identifier) @name) @definition.function
+
 (function_expression
-  name: (identifier) @function) @definition.function
+  name: (identifier) @name) @definition.function
+
+(method_definition
+  name: (property_identifier) @name) @definition.method
+
+; cffunction tag
+(cf_tag
+  (cf_start_tag
+    (cf_tag_name) @_cffunction
+    (tag_attributes
+      (attribute
+        (attribute_name) @_name
+        (_ (attribute_value) @name))))
+  (#eq? @_cffunction "function")
+  (#eq? @_name "name")) @definition.function
+
+; Function calls
+(call_expression
+  function: (identifier) @name) @reference.call
+
+(call_expression
+  function: (member_expression
+    property: (property_identifier) @name)) @reference.call
--- cfscript/queries/tags.scm
+++ cfscript/queries/tags.scm
@@ -1,2 +1,17 @@
+; Function definitions
+(function_declaration
+  name: (identifier) @name) @definition.function
+
 (function_expression
   name: (identifier) @name) @definition.function
+
+(method_definition
+  name: (property_identifier) @name) @definition.method
+
+; Function calls
+(call_expression
+  function: (identifier) @name) @reference.call
+
+(call_expression
+  function: (member_expression
+    property: (property_identifier) @name)) @reference.call
