bfd9aee9f5a183a128a9cb7eb27e028e245ed54c
Gareth Edwards <ghedwards@gmail.com>
2026-03-15 21:11:18 +1000

    Update Version 0.26.0

--- cfml/queries/injections.scm
+++ cfml/queries/injections.scm
@@ -5,3 +5,8 @@
 ((style_element
   (raw_text) @injection.content)
  (#set! injection.language "css"))
+
+((cf_script_tag
+  (cf_script_content) @injection.content)
+ (#set! injection.language "cfscript"))
+((cf_script_content) @injection.content (#set! injection.language "cfscript")) 
