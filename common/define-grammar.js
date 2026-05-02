c3302b80592b371a9fa14e7458535a42074e6610
Gareth Edwards <ghedwards@gmail.com>
2026-05-02 23:22:25 +1000

    Refinements
    - Add import to cf void tags
    - Fix comments `—` only applicable to cfquery
    - Create .zed/settings.json to avoid format on save issues

--- common/define-grammar.js
+++ common/define-grammar.js
@@ -1377,7 +1377,9 @@ module.exports = function defineGrammar(dialect) {
       // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
       comment: (_) => token(choice(
         seq('//', /[^\r\n\u2028\u2029]*/),
-        seq('--', /[^\r\n\u2028\u2029]*/),
+        ...(dialect !== 'cfquery' ? [
+          seq('--', /[^\r\n\u2028\u2029]*/),
+        ] : []),
         seq(
           '/*',
           /[^*]*\*+([^/*][^*]*\*+)*/,
