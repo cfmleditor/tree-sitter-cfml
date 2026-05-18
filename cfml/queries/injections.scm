e54b61332036b44ab3ead06202b7044cc0c59e21
Gareth Edwards <ghedwards@gmail.com>
2026-05-18 14:06:35 +1000

    Injection updates

--- cfml/queries/injections.scm
+++ cfml/queries/injections.scm
@@ -2,21 +2,31 @@
   (script_text) @injection.content)
  (#set! injection.language "javascript"))
 
-;; JS content inside CF tags within script elements
-((script_element
-  (_
-    (html_text) @injection.content))
- (#set! injection.language "javascript"))
+;; JS content inside CF tags within script elements (at varying nesting depths
+;; to cover cfif/cfelseif/cfelse branches and nested CF tags).
+;; Tree-sitter queries lack descendant matching, so explicit depths are needed.
+((script_element (_                         (html_text) @injection.content)) (#set! injection.language "javascript"))
+((script_element (_ (_                      (html_text) @injection.content))) (#set! injection.language "javascript"))
+((script_element (_ (_ (_                   (html_text) @injection.content)))) (#set! injection.language "javascript"))
+((script_element (_ (_ (_ (_                (html_text) @injection.content))))) (#set! injection.language "javascript"))
+((script_element (_ (_ (_ (_ (_             (html_text) @injection.content)))))) (#set! injection.language "javascript"))
+((script_element (_ (_ (_ (_ (_ (_          (html_text) @injection.content))))))) (#set! injection.language "javascript"))
+((script_element (_ (_ (_ (_ (_ (_ (_       (html_text) @injection.content)))))))) (#set! injection.language "javascript"))
+((script_element (_ (_ (_ (_ (_ (_ (_ (_    (html_text) @injection.content))))))))) (#set! injection.language "javascript"))
 
 ((style_element
   (style_text) @injection.content)
  (#set! injection.language "css"))
 
 ;; CSS content inside CF tags within style elements
-((style_element
-  (_
-    (html_text) @injection.content))
- (#set! injection.language "css"))
+((style_element (_                         (html_text) @injection.content)) (#set! injection.language "css"))
+((style_element (_ (_                      (html_text) @injection.content))) (#set! injection.language "css"))
+((style_element (_ (_ (_                   (html_text) @injection.content)))) (#set! injection.language "css"))
+((style_element (_ (_ (_ (_                (html_text) @injection.content))))) (#set! injection.language "css"))
+((style_element (_ (_ (_ (_ (_             (html_text) @injection.content)))))) (#set! injection.language "css"))
+((style_element (_ (_ (_ (_ (_ (_          (html_text) @injection.content))))))) (#set! injection.language "css"))
+((style_element (_ (_ (_ (_ (_ (_ (_       (html_text) @injection.content)))))))) (#set! injection.language "css"))
+((style_element (_ (_ (_ (_ (_ (_ (_ (_    (html_text) @injection.content))))))))) (#set! injection.language "css"))
 
 ((cf_script_tag
   (cf_script_content) @injection.content)
